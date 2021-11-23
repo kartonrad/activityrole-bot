fd86054ece3920d69b362d7c707d1553b55eace5
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2021-11-23 11:17:17 +0100

    Better logging

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const config = require("./config.json");
+const chakl = require("chalk");
 
-const discord = require("discord.js");
-const bot = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_PRESENCES]});
+const discord = require("discord.js");  // , discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_MEMBERS, 
+const chalk = require("chalk");
+const bot = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_PRESENCES]});
 
 bot.on("ready", () => {
+    console.log("Bot ONLINE!");
     console.log("Listening for Presence Changes...")
+    console.log("----------")
+    console.log(
+chalk`REVIEW your Security Settings::
+* The bot will {redBright.bold ${!config.createRoles?"NOT":""}} create Game-Roles, if they don't exist already.`);
+if (config.ignoreUnverifiedActivities)
+console.log(
+chalk`* The bot will ignore all activities that are not {blueBright.bold verified}.`);
+else 
+console.log(
+chalk`* The bot will process all activities, regardless of wether they're {blueBright.bold verified}.
+{red [WARNING]} this might allow members to get/create {redBright infinite/arbritrary} roles using the {yellow.bold "ACTIVITY SETTINGS > ACTIVITY STATUS"} Options in the discord settings`);
+if(config.ignoreUnverifiedActivities && config.createRoles)
+console.log(chalk`* These are the {green recommended} settings.`)
+console.log("----------\n Make sure critical roles that can grant access to sensitive content/moderator power are above the bot"+(config.ignoreUnverifiedActivities?" or have a name that doesnt match any verified game (e.g. random suffix)":"."));
+console.log("----------")
 });
+
 bot.on("presenceUpdate", async (prev, now) => {
-    console.log(`${now.user.username+"#"+now.user.discriminator} (on ${now.member.guild.name}) is now playing ${now.activities.map((activity) => activity.name)} (${now.activities.length})`)
+    console.log(chalk`{blueBright ${now.user.username+"#"+now.user.discriminator}} is now playing {yellow ${now.activities.map((activity) => activity.name)}} {grey (${now.activities.length})} {grey [${now.member.guild.name}]} `)
 
     if(now.member.manageable) {
         if(prev) {
@@ -21,7 +40,6 @@ bot.on("presenceUpdate", async (prev, now) => {
 
         var nowAct = config.ignoreUnverifiedActivities ? now.activities.filter(act => act.applicationId != null) : now.activities;
         var nowName= nowAct.map(act => act.name);
-        console.log(nowName);
 
         //add roles for new activities
         for (var i = 0; i < nowName.length; i++) {
@@ -35,18 +53,42 @@ bot.on("presenceUpdate", async (prev, now) => {
         for (var i = 0; i < prevName.length; i++) {
             var a = prevName[i];
             if(!nowName.includes(a)) {
-                var role = await pickRole(now.member.guild, a);
+                var role = await pickRole(now.member.guild, a, false);
                 if (role == null) continue;
                 now.member.roles.remove(role , "Stopped Playing Game"); 
             }
         }
     } else {
-        console.log("=> member is more priviledged than bot -> cant assign role")
+        console.log(chalk.red("   -> member is more priviledged than bot -> cant assign role"))
     }
 });
 
-bot.login(process.env.ACCESS_TOKEN || config.token); //config.token
 
+bot.login(process.env.ACCESS_TOKEN || config.token).catch((reason) => {
+    console.log(chalk`{redBright [LOGIN-ERROR]} Code: {grey ${reason.code}}`)
+    if (reason.code == "DISALLOWED_INTENTS") {
+        console.log(
+chalk`--------------------
+Please go to {blueBright https://discord.com/developers/applications/},
+click your app, click {bold.yellow 'Bot'} and make sure that {bold.yellow 'Presence Intent'} is enabled
+You can find it under the Section {bold.yellow 'Privileged Gateway Intents'}  
+
+You'll have to apply for bot verification if you plan to serve more than 100 servers.`);
+    }
+
+    if (reason.code == "TOKEN_INVALID") {
+        console.log(
+chalk`--------------------
+Please provide a {bold login-token} for your bot.
+
+To obtain one, go to {blueBright https://discord.com/developers/applications/},
+Click {bold.bgBlue.white 'New Application'}, give it a name and hit {bold.bgGreen.white 'Create'}.
+Navigate to {bold "Bot"} in the siedebar, than click {bold.bgBlue.white 'Add Bot'} and confirm.
+Then find the section {bold 'Token'} and click {bold.bgBlue.white 'Copy'}.
+
+Finally, paste it into {blueBright ./config.json} or set it as Enviroment Variable "{bold ACCESS_TOKEN}"`);
+    }
+}); //config.token
 
 //FUNCTIONS
 /**
@@ -55,8 +97,8 @@ bot.login(process.env.ACCESS_TOKEN || config.token); //config.token
  * @param {*} name 
  * @returns 
  */
-async function pickRole(guild, name) {
-    console.log("pick role for "+name)
+async function pickRole(guild, name, ass=true) {
+    //console.log("pick role for "+name)
     //role to be created 
     var desiredRolename = config.alias[name] || name;
 
@@ -66,15 +108,19 @@ async function pickRole(guild, name) {
     });
 
     if (role == null) { // create role
-        if(!config.createRoles) return null;
-        console.log(`creating role ${desiredRolename}`);
+        if(!config.createRoles) { 
+            console.log(chalk`  {yellow / role} "${desiredRolename}" {grey (Moderator must create this role first - security setting)}`)
+            return null
+        };
+        console.log(chalk.grey(`   -> creating role "${desiredRolename}"`));
         role = await guild.roles.create({
             name: desiredRolename,
             color: 'DEFAULT',
             reason: "Game Activity Detected"
         });
     } else {
-        console.log("found role");
+        if (ass) console.log(chalk`   {greenBright + role} {grey "${desiredRolename}"}`);
+        else console.log(chalk`   {red - role} {grey "${desiredRolename}"}`);
     }
 
     return role;
